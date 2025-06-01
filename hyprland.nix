5e5d173dc6a1ad8d06ca4cd463d2a86c4ec638c6
Luiz <luizcunha3@gmail.com>
2025-06-01 10:32:33 -0300

    set hyprland default

--- hyprland.nix
+++ hyprland.nix
@@ -8,6 +8,8 @@
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
   };
 
   hardware = {
